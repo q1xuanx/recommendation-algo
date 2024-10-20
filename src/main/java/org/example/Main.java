dd6d5bace8dcdbaa64c0ff5c3f958c22d6c85496
nhan <nhoang2929@gmail.com>
2024-10-20 22:18:50 +0700

    fix: recommend 1 and code train algo 2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import javafx.util.Pair;
 import org.apache.spark.ml.fpm.FPGrowth;
 import org.apache.spark.ml.fpm.FPGrowthModel;
 import org.apache.spark.sql.Dataset;
@@ -8,6 +9,7 @@ import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
 import org.apache.spark.sql.types.*;
 
+import java.sql.SQLOutput;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -19,23 +21,30 @@ public class Main {
                 .appName("SparkFPGrowth")
                 .getOrCreate();
         List<Row> data = Arrays.asList(
-                RowFactory.create(Arrays.asList("a b c".split(" "))),
                 RowFactory.create(Arrays.asList("a b c d".split(" "))),
+                RowFactory.create(Arrays.asList("a d e".split(" "))),
+                RowFactory.create(Arrays.asList("d e".split(" "))),
                 RowFactory.create(Arrays.asList("a b".split(" ")))
-        );
+                );
         StructType schema = new StructType(new StructField[]{new StructField(
                 "items", new ArrayType(DataTypes.StringType, true), false, Metadata.empty())
         });
         Dataset<Row> itemsDF = spark.createDataFrame(data, schema);
-        FPGrowthModel model = new FPGrowth().setItemsCol("items").setMinSupport(0.3).setMinConfidence(0.5).fit(itemsDF);
+        FPGrowthModel model = new FPGrowth().setItemsCol("items").setMinSupport(0.25).setMinConfidence(0.3).fit(itemsDF);
+        model.associationRules().show();
         List<String> IF = new ArrayList<>();
         for (char i = 'a'; i < 'c'; i++){
             IF.add(String.valueOf(i));
         }
         Map<String,Double> recommendList = algoRecommend1(model,IF);
-        for (Map.Entry<String, Double> entry : recommendList.entrySet()){
-            System.out.println(entry.getKey() + ": " + entry.getValue());
-        }
+        System.out.println("Recommendation Algo 1: " + recommendList);
+        List<String> data1 = new ArrayList<>();
+        data1.add("a b c d");
+        data1.add("a d e");
+        data1.add("d e");
+        data1.add("a b");
+        Map<Integer, List<Pair<Character, Double>>> trainRes = Train1(data1);
+        System.out.println("Train Algo 2: " + trainRes);
     }
     //Algo 1 based Association rules
     public static Map<String,Double> algoRecommend1(FPGrowthModel model, List<String> IF){
@@ -56,12 +65,40 @@ public class Main {
                     double c = row.getDouble(2);
                     long intr = countSize;
                     double ms = ((double) (intr * intr) / (antc.size() * IF.size()));
-                    double val = recommendList.get(f);
-                    val += val + c * ms;
-                    recommendList.put(f, val);
+                    recommendList.put(f, recommendList.get(f) + c * ms);
                 }
             }
         }
         return recommendList;
+    }
+    //Algo 2 based transactional item confidence;
+    public static Map<Integer, List<Pair<Character, Double>>> Train1 (List<String> data){
+        Map<Integer, List<Pair<Character, Double>>> tm = new HashMap<>();
+        data.replaceAll(s -> s.replace(" ", ""));
+        for (int i = 0; i < data.size(); i++){
+            if (!tm.containsKey(i)) {
+                tm.put(i, new ArrayList<>());
+            }
+            String m = data.get(i);
+            List<Pair<Character, Double>> saveConf = new ArrayList<>();
+            int cm = (int) data.stream().filter(s -> s.equals(m)).count();
+            for (int j = 0; j < m.length(); j++){
+                char f = m.charAt(j);
+                StringBuilder tm2 = new StringBuilder();
+                for (int z = 0; z < m.length(); z++){
+                    if (z == j) continue;
+                    tm2.append(m.charAt(z));
+                }
+                int cf = 0;
+                for (String datum : data) {
+                    String temp = datum.replace(Character.toString(f), "");
+                    if (temp.contains(tm2)) cf++;
+                }
+                Pair<Character, Double> pair = new Pair<>(f, (double) cf / cm);
+                saveConf.add(pair);
+            }
+            tm.put(i, new ArrayList<>(saveConf));
+        }
+        return tm;
     }
 }

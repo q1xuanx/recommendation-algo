9aee6f9f56f43950e0897169228ce8217e96a2c1
nhan <nhoang2929@gmail.com>
2024-11-27 19:43:21 +0700

    add: constrains

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,6 +7,7 @@ import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.RowFactory;
 import org.apache.spark.sql.SparkSession;
+import org.apache.spark.sql.sources.In;
 import org.apache.spark.sql.types.*;
 import scala.Char;
 
@@ -36,10 +37,17 @@ public class Main {
 //        Dataset<Row> itemsDF = spark.createDataFrame(data, schema);
 //        FPGrowthModel model = new FPGrowth().setItemsCol("items").setMinSupport(0.25).setMinConfidence(0.3).fit(itemsDF);
         //model.associationRules().show();
+        List<String> data = new ArrayList<>();
+        data.add("a, b, c, d");
+        data.add("a, d, e");
+        data.add("d, e");
+        data.add("a, b");
         List<String> IF = new ArrayList<>();
 //        for (char i = 'a'; i < 'c'; i++){
 //            IF.add(String.valueOf(i));
 //        }
+        IF.add("Tôm Sốt Cà");
+        IF.add("Chuối Già");
 //        IF.add("Cơm");
 //        IF.add("Gà Xào Rau Củ");
 //        Map<String,Double> recommendList = algoRecommend1(model,IF);
@@ -47,17 +55,33 @@ public class Main {
 //        for (Map.Entry<String, Double> entry : recommendList.entrySet()) {
 //            System.out.println(entry);
 //        }
-        RecommendModel recommendModel = new RecommendModel();
         List<String> data1 = readData("C:\\Users\\ADMIN\\Desktop\\AsRecomenResearch\\research\\src\\main\\java\\org\\example\\dataset.csv");
-        recommendModel.train(data1);
-        recommendModel.saveModel("/modelRecommend.ser");
-        System.out.println("Save Success");
+        System.out.println("Pairwise default: ");
+        Map<Pair<String, Double>, Map<String, Map<String,Double>>> trainSet = Train2(data1);
+        System.out.println("Recommend Pairwise: ");
+        Map<String,Double> rec1 = algoRecommend3(trainSet, IF);
+        System.out.println(rec1);
+        System.out.println("Pairwise constrain leased");
+        Map<Pair<String, Double>, Map<String, Map<String,Double>>> trainSet2 = trainConstraint(data1, IF);
+        System.out.println("Constraint as rules: ");
+        Map<String,Double> rec2 = recommendConstraint(trainSet2);
+        System.out.println(rec2);
+        //        recommendModel.saveModel("/modelRecommend.ser");
+//        System.out.println("Save Success");
         //setOfData(data1);
 //        InputStream modelStream = Main.class.getResourceAsStream("/modelRecommend.ser");
 //        recommendModel.loadModel(modelStream);
 //        for (Map.Entry<String, Double> entry : recommendModel.recommend(IF).entrySet()){
 //            System.out.println(entry.getKey() + ": " + entry.getValue());
 //        }
+    }
+    public static void printData(Map<Pair<String, Double>, Map<String, Map<String,Double>>> trainSet){
+        for (Map.Entry<Pair<String, Double>, Map<String, Map<String,Double>>> entry : trainSet.entrySet()) {
+            System.out.println(entry.getKey().getKey() + " " + entry.getKey().getValue());
+            for (Map.Entry<String, Map<String,Double>> entry1 : entry.getValue().entrySet()) {
+                System.out.println(entry1.getKey() + " " + entry1.getValue());
+            }
+        }
     }
     public static List<String> readData(String nameDataSet){
         List<String> data = new ArrayList<>();
@@ -148,6 +172,7 @@ public class Main {
         }
         return cf;
     }
+
     public static Map<String, Double> algoRecommend2(Map<Integer, List<Pair<String, Double>>> model, List<String> data){
         Map<String,Double> recommendList = new HashMap<>();
         for (Map.Entry<Integer, List<Pair<String, Double>>> entry : model.entrySet()){
@@ -200,12 +225,78 @@ public class Main {
                 }
             }
         }
+        tm = constructData(OD,CD);
+        return tm;
+    }
+    public static Map<Pair<String, Double>, Map<String, Map<String,Double>>> constructData(Map<String,Double> OD, Map<String, Map<String,Double>> CD){
+        Map<Pair<String, Double>, Map<String, Map<String,Double>>> tm = new HashMap<>();
         for (Map.Entry<String, Double> entry : OD.entrySet()){
             Pair<String, Double> mainPair = new Pair<>(entry.getKey(), entry.getValue());
             tm.put(mainPair, new HashMap<>());
             tm.get(mainPair).put(entry.getKey(), CD.get(entry.getKey()));
         }
         return tm;
+    }
+    //Constrain leased associations rules
+    public static Map<Pair<String, Double>, Map<String, Map<String,Double>>> trainConstraint(List<String> data, List<String> InputFood){
+        Map<Pair<String, Double>, Map<String, Map<String,Double>>> tm;
+        Map<String, Double> OD = new HashMap<>();
+        Map<String, Map<String, Double>> CD = new HashMap<>();
+        StringBuilder builder = new StringBuilder();
+        Set<String> ifFood = new HashSet<>(InputFood);
+        List<String> fFood = new ArrayList<>(ifFood);
+        for(String m: fFood){
+            builder.append(m).append(" ");
+        }
+        String query = builder.toString().trim();
+        OD.put(query, 0.0);
+        CD.put(query, new HashMap<>());
+        for (String m : data){
+            List<String> foods  = Arrays.stream(m.split(", ")).map(String::trim).collect(Collectors.toList());
+            boolean isContains = new HashSet<>(foods).containsAll(fFood);
+            if (isContains) {
+                List<String> remain = new ArrayList<>(foods);
+                remain.removeAll(fFood);
+                OD.put(query, OD.get(query) + 1);
+                for (String food : remain) {
+                    if (!CD.get(query).containsKey(food)) {
+                        CD.get(query).put(food, 1.0);
+                    } else {
+                        CD.get(query).put(food, CD.get(query).get(food) + 1);
+                    }
+                }
+            }
+        }
+        tm = constructData(OD,CD);
+        return tm;
+    }
+    public static Map<String,Double> recommendConstraint(Map<Pair<String, Double>, Map<String,Map<String,Double>>> model){
+        Map<String, Double> recommend = new HashMap<>();
+        Map<String, List<Double>> P = new HashMap<>();
+        Map<String, List<Double>> W = new HashMap<>();
+        Map<String,Double> OD = new HashMap<>();
+        Map<String, Map<String, Double>> CD = new HashMap<>();
+        for (Map.Entry<Pair<String, Double>, Map<String,Map<String,Double>>> entry : model.entrySet()){
+            OD.put(entry.getKey().getKey(), entry.getKey().getValue());
+            CD.put(entry.getKey().getKey(), entry.getValue().get(entry.getKey().getKey()));
+        }
+        for (Map.Entry<String, Map<String,Double>> entry : CD.entrySet()){
+            for (Map.Entry<String,Double> entry2 : entry.getValue().entrySet()){
+                if (!P.containsKey(entry2.getKey())) {
+                    P.put(entry2.getKey(), new ArrayList<>());
+                    W.put(entry2.getKey(), new ArrayList<>());
+                }
+                Double p = CD.get(entry.getKey()).get(entry2.getKey()) / OD.get(entry.getKey());
+                P.get(entry2.getKey()).add(p);
+                W.get(entry2.getKey()).add(OD.get(entry.getKey()));
+            }
+        }
+        for (String food : P.keySet()) {
+            Double pSum = P.get(food).stream().mapToDouble(Double::doubleValue).sum();
+            Double wSum = W.get(food).stream().mapToDouble(Double::doubleValue).sum();
+            recommend.put(food, (pSum * wSum));
+        }
+        return recommend;
     }
     public static Map<String, Double> algoRecommend3(Map<Pair<String, Double>, Map<String,Map<String,Double>>> model, List<String> data){
         Map<String, Double> recommendList = new HashMap<>();
